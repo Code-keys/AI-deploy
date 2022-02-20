fdee7858c2121c4d4f7f3c0314c43768330b4026
小程序 <SHECHAIN@OUTLOOK.COM>
2022-02-20 14:19:21 +0800

    INIT
    INIT

--- OpenCV/cvDarknet.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <opencv2/opencv.hpp>
-#include <opencv2/dnn.hpp>
-#include <fstream>
-#include <string>
-#include <opencv2/imgproc/imgproc_c.h>
-
-
-extern int test(float confidenceThreshold = 0.25, float nms = 0.45);
